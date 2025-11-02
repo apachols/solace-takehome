3010f9f2db9f2bb7a04066169a169ec50ab116f2
Adam Pacholski <adam.pacholski@rover.com>
2025-11-02 13:22:01 -0500

    Add typing for advocates

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,13 @@
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
 import { advocateData } from "../../../db/seed/advocates";
+import { Advocate } from "@/types/advocate";
 
 export async function GET() {
   // Uncomment this line to use a database
   // const data = await db.select().from(advocates);
 
-  const data = advocateData;
+  const data: Advocate[] = advocateData;
 
   return Response.json({ data });
 }
